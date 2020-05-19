67a427cad7e858b13bc9aeefb3900b53e26f07bb
Yuliya Yurkevich <juliana.ny2008@gmail.com>
2020-05-19 20:58:28 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -23,10 +23,10 @@ def monopoly_with_third_tier
      :two_pieces_owned => 50, 
      :three_pieces_owned => 100,
      :four_pieces_owned => 200}, 
-  :names => {:reading_railroad =>{}, 
-             :pennsylvania_railroad =>{},
-             :b_and_o_railroad =>{},
-             :shortline_railroad =>{}}
+  :names => {:reading_railroad =>{:mortgage_value =>100}, 
+             :pennsylvania_railroad =>{:morgage_value =>200},
+             :b_and_o_railroad =>{:morgage_value =>400},
+             :shortline_railroad =>{:morgage_value =>800}}
     
   } } 
   
