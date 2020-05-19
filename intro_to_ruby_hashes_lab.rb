696f2410b84dec594cf3689d547a4fa538066207
Yuliya Yurkevich <juliana.ny2008@gmail.com>
2020-05-19 21:01:25 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -44,9 +44,9 @@ def monopoly_with_fourth_tier
      :three_pieces_owned => 100,
      :four_pieces_owned => 200}, 
   :names => {:reading_railroad =>{:mortgage_value =>100}, 
-             :pennsylvania_railroad =>{:morgage_value =>200},
-             :b_and_o_railroad =>{:morgage_value =>400},
-             :shortline_railroad =>{:morgage_value =>800}}
+             :pennsylvania_railroad =>{:mortgage_value =>200},
+             :b_and_o_railroad =>{:mortgage_value =>400},
+             :shortline_railroad =>{:mortgage_value =>800}}
     
   } } 
   
