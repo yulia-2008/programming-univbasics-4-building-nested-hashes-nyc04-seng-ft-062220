ef5c63c49be26ae4a0f611ce588e587991eb5931
Yuliya Yurkevich <juliana.ny2008@gmail.com>
2020-05-19 21:00:25 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -26,7 +26,7 @@ def monopoly_with_third_tier
   :names => {:reading_railroad =>{}, 
              :pennsylvania_railroad =>{},
              :b_and_o_railroad =>{},
-             :shortline_railroad =>{}
+             :shortline_railroad =>{}}
     
   } } 
   
